d91c3df3a3ea7e21e8a1ce67b7f5d3b19bef1fc1
Tianzhu_Wang <stephen_einstein@icloud.com>
2024-04-01 01:57:48 +0800

    calendar and limits pt1

---
+++ api/em/calendar.py
@@ -0,0 +1,30 @@
+"""
+获取交易日期
+Created by Tianzhu Wang on 20240401
+"""
+import requests
+
+
+def trading_days(start_date='20190101'):
+    """
+    获取交易日历
+    :param start_date: 从哪一天开始算
+    :return: list[str]
+    """
+    url = f'http://dsfwt.10jqka.com.cn/bidding/api/tradingday/startdate/{start_date}'
+    # noinspection SpellCheckingInspection,HttpUrlsUsage
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7",
+        "Connection": "close",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Cookie": "sessionid=15358e32dfe6a519f28ededa1f21d8d22; jgbsessid=8c16101ce87ac72c4790f7b2bc159d1e; THSFT_USERID=zyzq3602; u_name=zyzq3602; userid=682593258; user=Onp5enEzNjAyOjo6Ojo1NCwxMTAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDA",
+        "Referer": "http://vis-free.10jqka.com.cn/billboard/ifind/html/index.html?theme=newgrey"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print(f'{response.text}，请求失败！')
+        return []
+    return response.json()['data']
--- api/jqka/limits.py
+++ api/jqka/limits.py
@@ -1,15 +1,17 @@
 import requests
 import pandas as pd
+import time
 from datetime import datetime
 
+# noinspection SpellCheckingInspection
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
     'Accept-Language': 'en-US,en;q=0.9',
     'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
     'DNT': '1',
-    'Host': 'eq.10jqka.com.cn',
+    'Host': 'data.10jqka.com.cn',
     'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-Mode': 'navigate',
     'Sec-Fetch-Site': 'none',
@@ -24,8 +26,86 @@ headers = {
 
 def daily_limits(date):
     """
-
-    :param date:
-    :return:
+    每天涨停股票的信息
+    :param date: 日期字符串，例如20240322
+    :return: pandas DataFrame
     """
-    url = 'https://data.10jqka.com.cn/dataapi/limit_up/limit_up_pool?page=1&limit=15&field=199112,10,9001,330323,330324,330325,9002,330329,133971,133970,1968584,3475914,9003,9004&filter=HS,GEM2STAR&date=20240328&order_field=330324&order_type=0&_=1711903787679'
+    url = 'https://data.10jqka.com.cn/dataapi/limit_up/limit_up_pool'
+    params = {
+        "page": "1",
+        "limit": "15",
+        "field": "199112,10,9001,330323,330324,330325,9002,330329,133971,133970,1968584,3475914,9003,9004",
+        "filter": "HS,GEM2STAR",
+        "date": date,
+        "order_field": "330324",
+        "order_type": "0",
+        "_": f'{int(time.time() * 1000)}'
+    }
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code != 200:
+        print(f'{response.text}，请求失败！')
+        return pd.DataFrame()
+    try:
+        data = response.json()['data']
+    except KeyError:
+        return pd.DataFrame()
+    count = data['page']['total']
+    info = data['info']
+    info = [{
+        "open_num": x['open_num'],
+        "tflim": x['first_limit_up_time'],
+        'tllim': x['last_limit_up_time'],
+        'code': x['code'],
+        'type': x['limit_up_type'],
+        'vlim': x['order_volume'],  # 封单量，多少手
+        'lim_srate': x['limit_up_suc_rate'],
+        'cirv': x['currency_value'],
+        'rate': x['change_rate'],
+        'tovr': x['turnover_rate'],
+        'reason': x['reason_type'],
+        'alim': x['order_amount'],  # 封单额，多少钱，单位元
+        'height': x['high_days'],  # 第几个板
+        'price': x['latest']
+    } for x in info]
+    res = [pd.DataFrame(info)]
+    for num in range(15, count, 15):
+        params = {
+            "page": f'{int(num / 15 + 1)}',
+            "limit": "15",
+            "field": "199112,10,9001,330323,330324,330325,9002,330329,133971,133970,1968584,3475914,9003,9004",
+            "filter": "HS,GEM2STAR",
+            "date": date,
+            "order_field": "330324",
+            "order_type": "0",
+            "_": f'{int(time.time() * 1000)}'
+        }
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code != 200:
+            print(f'{response.text}，循环中请求失败！')
+            continue
+        try:
+            data = response.json()['data']
+        except KeyError:
+            continue
+        count = data['page']['total']
+        info = data['info']
+        info = [{
+            "open_num": x['open_num'],
+            "tflim": x['first_limit_up_time'],
+            'tllim': x['last_limit_up_time'],
+            'code': x['code'],
+            'type': x['limit_up_type'],
+            'vlim': x['order_volume'],  # 封单量，多少手
+            'lim_srate': x['limit_up_suc_rate'],
+            'cirv': x['currency_value'],
+            'rate': x['change_rate'],
+            'tovr': x['turnover_rate'],
+            'reason': x['reason_type'],
+            'alim': x['order_amount'],  # 封单额，多少钱，单位元
+            'height': x['high_days'],  # 第几个板
+            'price': x['latest']
+        } for x in info]
+        res.append(pd.DataFrame(info))
+    res = pd.concat(res)
+    res.reset_index(inplace=True, drop=True)
+    return res
