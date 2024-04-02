8d453651b0e8cc6455e899a5305b81813019b857
TianZhu_Wang <stephen_einstein@iCloud.com>
2024-04-03 02:55:58 +0800

    multiprocessing

--- api/jqka/industry.py
+++ api/jqka/industry.py
@@ -3,8 +3,11 @@ Created on 2024.3.24
 获取同花顺行业（传统行业、概念板块、地域板块）每天的涨跌和各成分股
 """
 import requests
-import pandas as pd
 import datetime
+
+import pandas as pd
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 headers = {
     "Host": "dq.10jqka.com.cn",
@@ -93,11 +96,9 @@ def ind_stock_list(ind_code, date):
             return data
 
 
-def get_ind_list(date):
+def fetch_ind_list(date, page):
     """
-    获取指定日期的板块涨跌情况
-    :param date: 指定日期，请传入字符串，例如20240322
-    :return: pandas DataFrame
+    获取单页数据。
     """
     url = 'https://dq.10jqka.com.cn/interval_calculation/block_info/v1/get_block_list'
     payload = {
@@ -113,53 +114,48 @@ def get_ind_list(date):
         "type": 0,
         "page_info": {
             "page_size": 20,
-            "page": 1
+            "page": page
         }
     }
     response = requests.post(url, json=payload, headers=headers)
     if response.status_code != 200:
-        print(response.status_code, '请求失败！', f'请求参数：date={date}')
-        return pd.DataFrame()
+        print(response.status_code, '请求失败！', f'请求参数：date={date}, page={page}')
+        return []
     else:
-        data = response.json()['data']['list']
-        total = response.json()['data']['total']
-        if total < 20:
-            data = pd.DataFrame(data)
-            data.rename(columns={'turnover': 'amount', 'block_name': 'ind_name', 'margin_of_increase': 'pct_change',
-                                 'net_inflow_of_main_force': 'nf_main'}, inplace=True)
-            data.drop(columns=['stock_list'], inplace=True)
-            return data
-        else:
-            all_data = data.copy()
-            for i in range(20, total, 20):
-                payload = {
-                    "sort_info": {
-                        "sort_field": "0",
-                        "sort_type": "desc"
-                    },
-                    "history_info": {
-                        "history_type": "0",
-                        "end_date": f"{date}150000",
-                        "start_date": f"{date}093000"
-                    },
-                    "type": 0,
-                    "page_info": {
-                        "page_size": 20,
-                        "page": i / 20 + 1
-                    }
-                }
-                response = requests.post(url, json=payload, headers=headers)
-                if response.status_code != 200:
-                    print(response.status_code, '循环中请求失败！', f'请求参数：date={date}')
-                    continue
-                else:
-                    data = response.json()['data']['list']
-                    all_data += data
-            data = pd.DataFrame(all_data)
-            data.rename(columns={'turnover': 'amount', 'block_name': 'ind_name', 'margin_of_increase': 'pct_change',
-                                 'net_inflow_of_main_force': 'nf_main'}, inplace=True)
-            data.drop(columns=['stock_list'], inplace=True)
-            return data
+        data = response.json()['data']
+        return data
+
+
+def get_ind_list(date):
+    """
+    获取指定日期的板块涨跌情况，20220822开始可以获取
+    :param date: 指定日期，请传入字符串，例如20240322
+    :return: pandas DataFrame
+    """
+    initial_data = fetch_ind_list(date, 1)
+    if not initial_data:  # 如果初始请求失败，返回空DataFrame
+        return pd.DataFrame()
+    # noinspection PyTypeChecker
+    total = initial_data['total']
+    pages = total // 20 + (1 if total % 20 else 0)
+    # noinspection PyTypeChecker
+    all_data = initial_data['list'].copy()
+
+    with ThreadPoolExecutor(max_workers=10) as executor:  # 可以调整max_workers来优化性能
+        future_to_page = {executor.submit(fetch_ind_list, date, page): page for page in range(2, pages + 1)}
+        for future in as_completed(future_to_page):
+            page = future_to_page[future]
+            try:
+                data = future.result()['list']
+                all_data += data
+            except Exception as exc:
+                print(f'页面 {page} 请求产生了异常: {exc}')
+
+    data = pd.DataFrame(all_data)
+    data.rename(columns={'turnover': 'amount', 'block_name': 'ind_name', 'margin_of_increase': 'pct_change',
+                         'net_inflow_of_main_force': 'nf_main'}, inplace=True)
+    data.drop(columns=['stock_list'], inplace=True)
+    return data
 
 
 def ind_index_data(ind_code):
