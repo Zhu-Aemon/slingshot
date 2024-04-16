785455e04ce1aaba1ca5f986c85f2b3315e1ad55
TianZhu_Wang <stephen_einstein@iCloud.com>
2024-04-16 12:32:05 +0800

    tv scraper

--- api/tv/forex.py
+++ api/tv/forex.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import json
+import sqlite3
 
 
 def tv_forex_group(area='asia'):
@@ -32,3 +33,19 @@ def tv_forex_group(area='asia'):
     first_key = list(raw_data.keys())[0]
     records = [x['d'][2:11] for x in raw_data[first_key]['data']['screener']['data']['data']]
     return pd.DataFrame(records, columns=['pair', 'price', 'pct_change', 'change', 'bid', 'ask', 'high', 'low', 'rating'])
+
+
+def cn_forex_crossrates():
+    data = tv_forex_group()
+    data = data[data['pair'].apply(lambda x: 'cny' in x.lower())]
+    data.reset_index(inplace=True, drop=True)
+    return data
+
+
+def store_cn_data_live():
+    data = cn_forex_crossrates()
+    data.to_sql(con=sqlite3.connect('cn_fx_live.db'), if_exists='replace', name='live', index=False)
+
+
+if __name__ == '__main__':
+    store_cn_data_live()
---
+++ api/tv/world_economy.py
@@ -0,0 +1,76 @@
+import websocket
+import json
+import re
+import pandas as pd
+import sqlite3
+from collections import defaultdict
+
+
+# noinspection SpellCheckingInspection
+def get_econ_data(countries: list[str] = ['US', 'CN', 'EU', 'JP', 'DE', 'GB', 'FR', 'RU', 'CA', 'IT', 'AU'],
+                  indicators: list[str] = ['GDP', 'POP', 'GDPYY', 'INTR', 'IRYY', 'UR', 'CAG', 'GDG']):
+    all_indicators = [f'ECONOMICS:{x}{y}' for x in countries for y in indicators]
+    result = defaultdict(defaultdict)
+
+    def on_message(ws, message):
+        nonlocal result
+        messages = re.split(r'~m~\d{1,4}~m~', message)[1:]
+
+        def cum_data(res_iter):
+            nonlocal result
+            res_iter = json.loads(res_iter)
+            if res_iter['m'] != 'qsd':
+                return
+            res_iter = res_iter['p'][1]
+            indicator = res_iter['n']
+            field = res_iter['v']['short_name'] if 'short_name' in res_iter['v'].keys() else None
+            value = res_iter['v']['lp'] if 'lp' in res_iter['v'].keys() else None
+            desc = res_iter['v']['description'] if 'description' in res_iter['v'].keys() else None
+            change = res_iter['v']['ch'] if 'ch' in res_iter['v'].keys() else None
+            unit = res_iter['v']['value_unit_id'] if 'value_unit_id' in res_iter['v'].keys() else None
+            if field:
+                result[indicator]['field'] = field
+            if value:
+                result[indicator]['value'] = value
+            if desc:
+                result[indicator]['desc'] = desc
+            if change:
+                result[indicator]['change'] = change
+            if unit:
+                result[indicator]['unit'] = unit
+
+        for message_iter in messages:
+            cum_data(message_iter)
+        messages = pd.DataFrame({k: dict(v) for k, v in dict(result).items()}).T
+        messages.dropna(inplace=True, subset=['field'])
+        if len(messages) == len(all_indicators):
+            messages.to_sql(con=sqlite3.connect('econ_live.db'), if_exists='replace', index=False, name='live')
+            ws.close()
+
+    def on_open(ws):
+        print("Connection opened")
+        # 发送消息
+        # noinspection SpellCheckingInspection
+        payload = ["qs_Xn90P2uiDHrm"] + all_indicators
+        payload = '{"m":"quote_add_symbols","p":["' + '","'.join(payload) + '"]}'
+        payload_length = len(payload)
+        messages = [
+            '~m~36~m~{"m":"set_data_quality","p":["low"]}',
+            '~m~54~m~{"m":"set_auth_token","p":["unauthorized_user_token"]}',
+            '~m~34~m~{"m":"set_locale","p":["en","US"]}',
+            '~m~52~m~{"m":"quote_create_session","p":["qs_Xn90P2uiDHrm"]}',
+            '~m~453~m~{"m":"quote_set_fields","p":["qs_Xn90P2uiDHrm","base-currency-logoid","ch","chp","currency-logoid","currency_code","currency_id","base_currency_id","current_session","description","exchange","format","fractional","is_tradable","language","local_description","listed_exchange","logoid","lp","lp_time","minmov","minmove2","original_name","pricescale","pro_name","short_name","type","typespecs","update_mode","volume","variable_tick_size","value_unit_id"]}',
+            f'~m~{payload_length}~m~{payload}'
+        ]
+        for msg in messages:
+            ws.send(msg)
+
+    websocket.enableTrace(False)
+    ws_instance = websocket.WebSocketApp(
+        "wss://data.tradingview.com/socket.io/websocket?from=markets%2Fworld-economy%2F&date=2024_04_12-11_22",
+        on_message=on_message, on_open=on_open)
+    ws_instance.run_forever()
+
+
+if __name__ == "__main__":
+    get_econ_data()
