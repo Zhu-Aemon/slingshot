7bb5a803bb23f0e58a5f119a81ab32b1068d2b79
TianZhu_Wang <stephen_einstein@iCloud.com>
2024-11-21 22:37:17 +0800

    get jqka industry complete kline data

--- api/jqka/industry.py
+++ api/jqka/industry.py
@@ -5,10 +5,13 @@ Created on 2024.3.24
 import requests
 import datetime
 import random
-
+import json
 import pandas as pd
+import numpy as np
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from get_pc_cookie import get_cookie_pc
+from itertools import chain
 
 headers = {
     "Host": "dq.10jqka.com.cn",
@@ -159,9 +162,9 @@ def get_ind_list(date):
     return data
 
 
-def ind_index_data(ind_code):
+def ind_k_recent(ind_code):
     """
-    获取一个板块的指数k线
+    获取一个板块的指数k线，但是只能获取最近一些
     :param ind_code: 板块代码
     :return: pandas DataFrame
     """
@@ -176,3 +179,51 @@ def ind_index_data(ind_code):
         del data['yesterday_close_price']
         data.rename(columns={'high_price': 'high', 'low_price': 'low', 'open_price': 'open', 'close_price': 'close'}, inplace=True)
         return data
+
+
+def ind_k_all(ind_code):
+    """
+    获取一个板块所有的k线
+    注意这里用的是同花顺网页版的API，这也就意味着cookie的设置必然和手机版的有所不同
+    :param ind_code: 同花顺板块代码
+    :return: DataFrame
+    """
+    # noinspection SpellCheckingInspection
+    cookies_pc = {
+        'historystock': ind_code,
+        'spversion': '20130314',
+        'v': get_cookie_pc(),
+    }
+    headers_pc = {
+        'Accept': '*/*',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Referer': 'https://stockpage.10jqka.com.cn/',
+        'Sec-Fetch-Dest': 'script',
+        'Sec-Fetch-Mode': 'no-cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+        'sec-ch-ua': '"Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+    }
+    response = requests.get(f'https://d.10jqka.com.cn/v6/line/bk_{ind_code}/01/all.js', cookies=cookies_pc, headers=headers_pc)
+    data = response.text.lstrip(f'quotebridge_v6_line_bk_{ind_code}_01_all(').rstrip(')')
+    data = json.loads(data)
+    del data['afterVolumn']
+    price = data['price'].split(',')
+    price = [price[i * 4: (i + 1) * 4] for i in range(len(price) // 4)]
+    price = np.array([[int(x[0]), int(x[0]) + int(x[1]), int(x[0]) + int(x[2]), int(x[0]) + int(x[3])] for x in price]) / data['priceFactor']  # 最低价，开盘价，最高价，收盘价
+    volume = [int(x) for x in data['volumn'].split(',')]
+    year = data['sortYear']
+    year = [[str(x[0])] * x[1] for x in year]
+    year = list(chain.from_iterable(year))
+    dates = data['dates'].split(',')
+    dates = [year[i] + dates[i] for i in range(len(dates))]
+    data = pd.DataFrame(price, columns=['low', 'open', 'high', 'close'])
+    data['date'] = dates
+    data['date'] = data['date'].apply(lambda x: datetime.datetime.strptime(x, '%Y%m%d'))
+    data['volume'] = volume
+    data = data[['date', 'open', 'low', 'high', 'close', 'volume']]
+    return data
