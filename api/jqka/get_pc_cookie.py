02724c1a967307134dab27072de9a98e585998bc
TianZhu_Wang <stephen_einstein@iCloud.com>
2025-01-12 12:08:50 +0800

    reformat js file location

--- api/jqka/get_pc_cookie.py
+++ api/jqka/get_pc_cookie.py
@@ -5,7 +5,7 @@ import execjs
 
 
 def get_cookie_pc():
-    with open('pc_cookie.js', encoding='utf8') as f:
+    with open('../../js/jqka_v.js', encoding='utf8') as f:
         js_func = execjs.compile(f.read())
 
     v = js_func.call('get_v')
