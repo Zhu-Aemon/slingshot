f045935a338e22970999a7e15068d3a94d35b59c
TianZhu_Wang <stephen_einstein@iCloud.com>
2025-01-15 13:44:08 +0800

    fix js path

--- api/jqka/get_pc_cookie.py
+++ api/jqka/get_pc_cookie.py
@@ -2,10 +2,16 @@
 生成同花顺所用的v cookie
 """
 import execjs
+import os
 
 
 def get_cookie_pc():
-    with open('../../js/jqka_v.js', encoding='utf8') as f:
+    # Get the absolute path to this file's directory
+    module_dir = os.path.dirname(os.path.abspath(__file__))
+    # Construct the path to your .js file relative to this Python file
+    js_path = os.path.join(module_dir, 'js/jqka_v.js')
+
+    with open(js_path, encoding='utf8') as f:
         js_func = execjs.compile(f.read())
 
     v = js_func.call('get_v')
